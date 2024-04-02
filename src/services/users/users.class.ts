8554788728a898a2bfa507b77c50be275b9c315b
ejmudrak <ejmudrak@gmail.com>
2024-04-02 15:18:47 -0400

    Adds multi delete for users service

--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -22,6 +22,7 @@ export const getOptions = (app: Application): KnexAdapterOptions => {
   return {
     paginate: app.get('paginate'),
     Model: app.get('postgresqlClient'),
-    name: 'users'
+    name: 'users',
+    multi: ['remove'],
   }
 }
